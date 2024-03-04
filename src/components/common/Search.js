f3306abba541b122beb20382f6546cb2f22b19af
Ashu Singla <ashusingla884@gmail.com>
2024-03-04 23:40:15 +0530

    feat: add new prop

--- src/components/common/Search.js
+++ src/components/common/Search.js
@@ -2,19 +2,24 @@ import React from "react";
 import searchIcon from "../../assets/icons/search.svg";
 import targetIcon from "../../assets/icons/target.png";
 
-function Search({ value, handleChange, placeholder = "Search for places..." }) {
+function Search({
+  value,
+  handleChange,
+  handleSubmit,
+  placeholder = "Search for places...",
+}) {
   return (
     <div className="flex">
       <img alt="search-icon" src={searchIcon} className="w-5" />
       <input
         type="search"
         id="default-search"
-        className="block w-full p-4 ps-10 text-sm text-black rounded-l dark:placeholder-black focus:outline-none"
+        className="block w-full pr-4 py-4 ps-3 text-sm text-black rounded-l dark:placeholder-black focus:outline-none"
         placeholder={placeholder}
         value={value}
-        handleChange={handleChange}
+        onChange={handleChange}
       />
-      <button type="submit">
+      <button type="submit" onClick={handleSubmit}>
         <img alt="search-icon" src={targetIcon} className="w-4" />
       </button>
     </div>
