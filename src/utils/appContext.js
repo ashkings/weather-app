32ae4cf3fcd023d6108e85c5ab74fe7c3ad33c63
Ashu Singla <ashusingla884@gmail.com>
2024-03-05 01:35:33 +0530

    feat: add new values to context

--- src/utils/appContext.js
+++ src/utils/appContext.js
@@ -5,14 +5,24 @@ export const appContext = createContext(null);
 function AppContextProvider({ children }) {
   const [metricType, setMetricType] = useState("metric");
   const [weatherDetails, setWeatherDetails] = useState({});
+  const [city, setCity] = useState("");
   const providerValue = useMemo(
     () => ({
       metricType,
       setMetricType,
       weatherDetails,
       setWeatherDetails,
+      city,
+      setCity,
     }),
-    [metricType, setMetricType, weatherDetails, setWeatherDetails]
+    [
+      metricType,
+      setMetricType,
+      weatherDetails,
+      setWeatherDetails,
+      city,
+      setCity,
+    ]
   );
   return (
     <appContext.Provider value={providerValue}>{children}</appContext.Provider>
